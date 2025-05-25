91e955f05bfb43ec08f2785972ef204c27dceba1
RafaGVJ <rafael_guerreiro@eticalgarve.com>
2025-05-25 16:40:33 +0100

    TEsting

---
+++ Assets/Script/Animations/StateMachine.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+    private IState currentState;
+
+    public void ChangeState(IState newState)
+    {
+        //currentState?
+    }
+}
