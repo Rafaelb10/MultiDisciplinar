a17df502cf29fd4f504ff677e3dc9e06a955f508
RafaGVJ <rafael_guerreiro@eticalgarve.com>
2025-05-30 21:39:14 +0100

    .

---
+++ Assets/Script/Levels1.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Levels1 : MonoBehaviour
+{
+    
+
+   
+    public void LoadScene1()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
